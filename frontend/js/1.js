05531f49bfc9e3f27c37233d4918b4a1db510d90
Andini Pratiwi <andini.pratiwi088@gmail.com>
2018-12-11 18:02:54 +0700

    Basic UI Template - edit profile

--- frontend/js/1.js
+++ frontend/js/1.js
@@ -27,3 +27,21 @@ function SidebarCollapse () {
     // Collapse/Expand icon
     $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
 }
+
+
+//IMAGE UPLOAD
+// $("input").change(function(e) {
+
+//     for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+        
+//         var file = e.originalEvent.srcElement.files[i];
+        
+//         var img = document.createElement("img");
+//         var reader = new FileReader();
+//         reader.onloadend = function() {
+//             img.src = reader.result;
+//         }
+//         reader.readAsDataURL(file);
+//         $("input").after(img);
+//     }
+// });
