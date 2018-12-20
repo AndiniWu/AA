bd2cd7641316ed55cb42a5a32fcef7718718dbe6
azhang0411 <azhang0411@yahoo.com>
2018-12-20 21:02:03 +0700

    CMT 006.1 - Tidying the code

--- ois/src/main/resources/static/js/ajax.js
+++ ois/src/main/resources/static/js/ajax.js
@@ -1,4 +1,4 @@
-function agetUserbyId(id){
+function agetUserById(id){
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/api/users/'+id,
@@ -8,8 +8,19 @@ function agetUserbyId(id){
         dataType:"json",
         success: function (data) {
             console.log("yes. data: " + data)
-            $('#name').val(data.name);//terakhir sampai sini
-        },
+            $("#nik").val(parseInt(data.nik));
+            $('#name').val(data.name);
+            $('#email').val(data.email);
+            $('#password').val(data.password);
+            $('#division').val(data.division);
+            $('#position').val(data.position);
+            $('#cnumber').val(parseInt(data.cnumber));
+            $('#address').val(data.address);
+            if(data.role==1){$('input[name="optradio"]')[0].prop('checked',true)}
+            if(data.superior!=null){
+            $('#superiorList').val(data.superior.email);}
+            agetAllSuperiors();
+            },
         error: function (error) {
             console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
         }
@@ -76,6 +87,80 @@ function agetAllUsers(){
                     console.log(data);
                 }
             }
+        },
+        error: function (error) {
+            console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
+        }
+    });
+}
+
+function agetAllSuperiors(){
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/api/users/superiors',
+        headers: {
+            "Content-Type": "application/json", "Accept": "application/json"
+        },
+        dataType:"json",
+        success: function (data) {
+            console.log("yes. data: " + data)
+            if (data) {
+                var len = data.length;
+                var txt = "";
+                if (len > 0) {
+                    for(var i=0;i<len;i++){
+                        if(data[i].name && data[i].email){
+                            txt += '<option value='+data[i].id+'>'+data[i].email+'</option>'
+                        }
+                    }
+                    if(txt != ""){
+                        $("#superiorList").append(txt);
+                    }
+                    alert("Success :"+data);
+                }
+            }
+        },
+        error: function (error) {
+            console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
+        }
+    });
+}
+
+function aaddUser(){
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var name = document.getElementById("name").value;
+    var role = document.querySelector('input[name="optradio"]:checked').value;
+    var superior = document.getElementById("superiorList").value;
+    var nik = document.getElementById("nik").value;
+    var division= document.getElementById("division").value;
+    var position = document.getElementById("position").value;
+    var cnumber = document.getElementById("cnumber").value;
+    var address = document.getElementById("address").value;
+
+    var txt = '{"name": "' + name + '", "email": "' + email + '", "password": "' + password + '", "role": ' + role + ', "nik": "' + nik + '", "division": "' + division + '", "position": "' + position + '", "cnumber": "' + cnumber + '", "address": "' + address + '"}';
+    if(superior!=""){
+        txt = '{"name": "' + name + '", "email": "' + email + '", "password": "' + password + '", "role": ' + role +', "nik": "' + nik + '", "division": "' + division + '", "position": "' + position + '", "cnumber": "' + cnumber + '", "address": "' + address + '", "superior": { "id" : '+superior+'}}'
+    }
+    console.log(txt);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/api/users',
+        data: txt,
+        headers: {
+            "Content-Type": "application/json", "Accept": "application/json"
+        },
+        dataType: "json", //to parse string into JSON object,
+        success: function (data) {
+            var msg = "Call Success. result: ";
+            if (data === false) {
+                msg += "There is already a user registered with the email provided"
+            }
+            else {
+                msg += "Successed to save user";
+            }
+            console.log(msg);
+            //alert("Success :"+data)
         },
         error: function (error) {
             console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
--- ois/src/main/resources/static/js/index.js
+++ ois/src/main/resources/static/js/index.js
@@ -2,7 +2,6 @@ var user = getCookie("user");
 user = user.split(',');
 document.getElementById("profile").innerText = user[2];
 
-
 var isi= document.getElementById("isi")
 $('#logout').click(function(e){
     e.preventDefault();
@@ -12,7 +11,8 @@ $('#logout').click(function(e){
 
 function editUser(userId){
     isi.innerHTML=addUser();
-    getUserbyId(userId);
+    agetAllSuperiors();
+    agetUserById(userId);
 };
 function deleteUser(userId){
 
@@ -36,36 +36,35 @@ $('#addUser').click(function(e){
     e.preventDefault();
     // $( document ).ready(function() {
     //     console.log( "ready!" );
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8080/api/users/superiors',
-        headers: {
-            "Content-Type": "application/json", "Accept": "application/json"
-        },
-        dataType:"json",
-        success: function (data) {
-            console.log("yes. data: " + data)
-            if (data) {
-                var len = data.length;
-                var txt = "";
-                if (len > 0) {
-                    for(var i=0;i<len;i++){
-                        if(data[i].name && data[i].email){
-                            txt += '<option value='+data[i].id+'>'+data[i].email+'</option>'
-                        }
-                    }
-                    if(txt != ""){
-                        $("#superiorList").append(txt);
-                    }
-                    alert("Success :"+data);
-                }
-            }
-        },
-        error: function (error) {
-            console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
-        }
-    });
-
+    // $.ajax({
+    //     type: 'GET',
+    //     url: 'http://localhost:8080/api/users/superiors',
+    //     headers: {
+    //         "Content-Type": "application/json", "Accept": "application/json"
+    //     },
+    //     dataType:"json",
+    //     success: function (data) {
+    //         console.log("yes. data: " + data)
+    //         if (data) {
+    //             var len = data.length;
+    //             var txt = "";
+    //             if (len > 0) {
+    //                 for(var i=0;i<len;i++){
+    //                     if(data[i].name && data[i].email){
+    //                         txt += '<option value='+data[i].id+'>'+data[i].email+'</option>'
+    //                     }
+    //                 }
+    //                 if(txt != ""){
+    //                     $("#superiorList").append(txt);
+    //                 }
+    //                 alert("Success :"+data);
+    //             }
+    //         }
+    //     },
+    //     error: function (error) {
+    //         console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
+    //     }
+    // });
     isi.innerHTML=addUser(); //mengubah isi konten dengan halaman adduser
     $('input:radio').click(function() {
         $("#superiorList").prop("disabled",true);
@@ -74,45 +73,46 @@ $('#addUser').click(function(e){
         }
     });
     $("#register").click(function () {
-        var email = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
-        var name = document.getElementById("name").value;
-        var role = document.querySelector('input[name="optradio"]:checked').value;
-        var superior = document.getElementById("superiorList").value;
-        var nik = document.getElementById("nik").value;
-        var division= document.getElementById("division").value;
-        var position = document.getElementById("position").value;
-        var cnumber = document.getElementById("cnumber").value;
-        var address = document.getElementById("address").value;
-
-        var txt = '{"name": "' + name + '", "email": "' + email + '", "password": "' + password + '", "role": ' + role + ', "nik": "' + nik + '", "division": "' + division + '", "position": "' + position + '", "cnumber": "' + cnumber + '", "address": "' + address + '"}';
-        if(superior!=""){
-            txt = '{"name": "' + name + '", "email": "' + email + '", "password": "' + password + '", "role": ' + role +', "nik": "' + nik + '", "division": "' + division + '", "position": "' + position + '", "cnumber": "' + cnumber + '", "address": "' + address + '", "superior": { "id" : '+superior+'}}'
-        }
-        console.log(txt);
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:8080/api/users',
-            data: txt,
-            headers: {
-                "Content-Type": "application/json", "Accept": "application/json"
-            },
-            dataType: "json", //to parse string into JSON object,
-            success: function (data) {
-                var msg = "Call Success. result: ";
-                if (data === false) {
-                    msg += "There is already a user registered with the email provided"
-                }
-                else {
-                    msg += "Successed to save user";
-                }
-                console.log(msg);
-                //alert("Success :"+data)
-            },
-            error: function (error) {
-                console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
-            }
-        });
+        // var email = document.getElementById("email").value;
+        // var password = document.getElementById("password").value;
+        // var name = document.getElementById("name").value;
+        // var role = document.querySelector('input[name="optradio"]:checked').value;
+        // var superior = document.getElementById("superiorList").value;
+        // var nik = document.getElementById("nik").value;
+        // var division= document.getElementById("division").value;
+        // var position = document.getElementById("position").value;
+        // var cnumber = document.getElementById("cnumber").value;
+        // var address = document.getElementById("address").value;
+        //
+        // var txt = '{"name": "' + name + '", "email": "' + email + '", "password": "' + password + '", "role": ' + role + ', "nik": "' + nik + '", "division": "' + division + '", "position": "' + position + '", "cnumber": "' + cnumber + '", "address": "' + address + '"}';
+        // if(superior!=""){
+        //     txt = '{"name": "' + name + '", "email": "' + email + '", "password": "' + password + '", "role": ' + role +', "nik": "' + nik + '", "division": "' + division + '", "position": "' + position + '", "cnumber": "' + cnumber + '", "address": "' + address + '", "superior": { "id" : '+superior+'}}'
+        // }
+        // console.log(txt);
+        // $.ajax({
+        //     type: 'POST',
+        //     url: 'http://localhost:8080/api/users',
+        //     data: txt,
+        //     headers: {
+        //         "Content-Type": "application/json", "Accept": "application/json"
+        //     },
+        //     dataType: "json", //to parse string into JSON object,
+        //     success: function (data) {
+        //         var msg = "Call Success. result: ";
+        //         if (data === false) {
+        //             msg += "There is already a user registered with the email provided"
+        //         }
+        //         else {
+        //             msg += "Successed to save user";
+        //         }
+        //         console.log(msg);
+        //         //alert("Success :"+data)
+        //     },
+        //     error: function (error) {
+        //         console.log('errorCode: ' + error.status + ' . Message: ' + error.responseText);
+        //     }
+        // });
+        aaddUser()
     });
 
 });
